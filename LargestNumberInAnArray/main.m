e69fe5cbea0bee679e6c8b6788cedf9c726d9108
KrazeeKoder <tony.tulai@gmail.com>
2015-12-30 00:36:02 -0500

    Initial Commit

---
+++ LargestNumberInAnArray/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LargestNumberInAnArray
+//
+//  Created by Anthony Tulai on 2015-12-30.
+//  Copyright © 2015 Anthony Tulai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
